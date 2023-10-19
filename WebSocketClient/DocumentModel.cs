1865e56d87cb692c2dfe01dd5101c59a2bac4821
Emre Tomruk <emre.tomruk@fortiustech.com.tr>
2023-10-19 17:26:35 +0300

    Add project files.

---
+++ WebSocketClient/DocumentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebSocketClient
+{
+	public class TabletDocumentModel
+	{
+		public string EncryptedApplicationId { get; set; }
+		public List<DocumentModel> DigitalSignatureDocuments { get; set; }
+	}
+
+	public class DocumentModel
+	{
+		public byte DigitalSignatureDocumentId { get; set; }
+		public byte DocumentLanguageId { get; set; }
+	}
+}
---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using WebSocketClient;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5000/digitalsignaturehub")
+    .WithAutomaticReconnect()
+    .Build();
+
+connection.Reconnecting += sender =>
+{
+    ReConnecting();
+    return Task.CompletedTask;
+};
+
+OpenConnection();
+Console.ReadLine();
+
+void OpenConnection()
+{
+    connection.On<string>("SetConnection", connectionId =>
+    {
+        connection.InvokeAsync("SaveClient", connectionId, "test");
+        Console.WriteLine($"Connected: {DateTime.Now}");
+    });
+
+    connection.On<TabletDocumentModel>("ReceiveDocument", model =>
+    {
+	    //connection.InvokeAsync("SaveClient", connectionId, "test");
+	    Console.WriteLine($"Connected: {DateTime.Now}");
+    });
+
+	try
+    {
+        connection.StartAsync();
+    }
+    catch (Exception ex)
+    {
+        //
+    }
+}
+
+void ReConnecting()
+{
+    Console.WriteLine($"Reconnecting: {DateTime.Now}");
+}
