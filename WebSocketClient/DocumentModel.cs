88c93b68b140392eb1252cd1c6878b370f51a7bd
Emre Tomruk <emre.tomruk@fortiustech.com.tr>
2023-12-19 18:52:30 +0300

    save document method added

--- WebSocketClient/DocumentModel.cs
+++ WebSocketClient/DocumentModel.cs
@@ -1,17 +1,56 @@
-﻿namespace WebSocketClient
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSocketClient
 {
-	public class TabletDocumentModel
+	public sealed class TabletDocumentModel
 	{
 		public int ApplicationId { get; set; }
         public int CreatedBy { get; set; }
+        public byte MinorApplicantParentTypeId { get; set; }
         public List<DocumentModel> DigitalSignatureDocuments { get; set; }
-	}
+        public List<MinorApplicantParentApiRequest> MinorApplicantParents { get; set; }
+    }
 
-	public class DocumentModel
+    public sealed class DocumentModel
 	{
+        [Required]
 		public byte DigitalSignatureDocumentId { get; set; }
 		public byte DocumentLanguageId { get; set; }
+
+        [Required]
         public string Html { get; set; }
+        public List<DocumentSignatureRequestModel> Signatures { get; set; }
+    }
+
+    public sealed class MinorApplicantParentApiRequest
+    {
+        public string Name { get; set; }
+    }
+
+    public sealed class DocumentRequestModel 
+    {
+        public List<DocumentSignatureRequestModel> Signatures { get; set; }
+    }
+
+    public sealed class DocumentSignatureRequestModel
+    {
+        public string Name { get; set; }
         public string Signature { get; set; }
+    }
+
+    public sealed class SaveDocumentsRequestModel 
+    {
+        public int ApplicationId { get; set; }
+        public int CreatedBy { get; set; }
+        public List<DigitalSignatureDocumentRequestModel> DigitalSignatureDocuments { get; set; }
+    }
+
+    public sealed class DigitalSignatureDocumentRequestModel
+    {
+        public byte DigitalSignatureDocumentId { get; set; }
+        public byte DocumentLanguageId { get; set; }
+        public string Html { get; set; }
+
+        public List<DocumentSignatureRequestModel> Signatures { get; set; }
     }
 }
--- WebSocketClient/Program.cs
+++ WebSocketClient/Program.cs
@@ -19,9 +19,14 @@ void OpenConnection()
 {
     connection.On<string>("SetConnection", connectionId =>
     {
-        connection.InvokeAsync("SaveClient", connectionId, "hostname");
+        connection.InvokeAsync("SaveClient", connectionId, "test20", "1.0.0");
 
         Console.WriteLine($"Connected: {DateTime.Now}");
+    });
+
+    connection.On<string>("GetDeviceName", tabletName =>
+    {
+        Console.WriteLine($"Tablet-name: {tabletName}");
     });
 
     connection.On<TabletDocumentModel>("ReceiveDocument", async documents =>
@@ -32,12 +37,46 @@ void OpenConnection()
 
         var apiRequestHeader = SetRequestHeader(documentRequest.DocumentLanguageId.ToString());
 
+        var apiRequest = new DocumentRequestModel
+        { 
+            Signatures = documents.MinorApplicantParents.Select(p => new DocumentSignatureRequestModel
+            {
+                Name = p.Name
+            }).ToList() 
+        };
+
 		var apiResponse = await HttpClientHelper
-			  .GetAsync<ApiResponse<ApplicationEntryFormApiResponse>>
-			  ("/api/DigitalSignature/ExplicitConsentText/" + documents.ApplicationId + "/" + documentRequest.DocumentLanguageId, "https://localhost:7071", apiRequestHeader)
+			  .PostAsJsonAsync<ApiResponse<ApplicationEntryFormApiResponse>>
+			  (apiRequest, "/api/DigitalSignature/CargoReceiptForm/" + documents.ApplicationId + "/" + documentRequest.DocumentLanguageId, "https://localhost:7071", apiRequestHeader)
 			  .ConfigureAwait(false);
 
-		Console.WriteLine($"Connected: {DateTime.Now}");
+		Console.WriteLine($"document received: {DateTime.Now}, documentId:{documentRequest.DigitalSignatureDocumentId}");
+
+        var saveDocumentApiRequest = new SaveDocumentsRequestModel
+        {
+            ApplicationId = documents.ApplicationId,
+            CreatedBy = documents.CreatedBy,
+            DigitalSignatureDocuments = new List<DigitalSignatureDocumentRequestModel>
+            {
+                new DigitalSignatureDocumentRequestModel
+                {
+                    DigitalSignatureDocumentId = documentRequest.DigitalSignatureDocumentId,
+                    Html = apiResponse.Data.Html,                    
+                    Signatures = apiRequest.Signatures.Select(p => new DocumentSignatureRequestModel
+                    {
+                        Name = p.Name,
+                        Signature = File.ReadAllText(@"C:\\Users\\Lenovo\\source\\repos\\WebSocketClient\\WebSocketClient\\signature.txt")
+                    }).ToList()
+                }              
+            }
+        };
+
+        var saveProcess = await HttpClientHelper
+              .PostAsJsonAsync<ApiResponse<SaveDocumentsApiResponse>>
+              (saveDocumentApiRequest, "/api/DigitalSignature/SaveDocuments", "https://localhost:7071", apiRequestHeader)
+              .ConfigureAwait(false);
+
+        Console.WriteLine($"document saved: {DateTime.Now}, isSuccess: {saveProcess.IsSuccess}");
     });
 
 	try
@@ -55,7 +94,7 @@ void ReConnecting()
     Console.WriteLine($"Reconnecting: {DateTime.Now}");
 }
 
-Dictionary<string, string>? SetRequestHeader(string languageId)
+Dictionary<string, string> SetRequestHeader(string languageId)
 {
 	return new Dictionary<string, string>
 	{
